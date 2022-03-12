f02727e02254459b7911f35d15a5401244cd58f2
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-12 13:16:26 -0500

    Add fonts to assets, Update Categories Screen to use new fonts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'categories_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +12,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              subtitle1: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+              subtitle2: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );

077d6db3cfeaee4e6546eb8d51113f5673b7839d
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-14 18:53:56 -0400

    Update TabBar so the correct title displays in the AppBar title.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -12,9 +12,9 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Widget> _pages = [
-    CategoriesScreen(),
-    FavoritesScreen(),
+  final List<Map<String?, Object?>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Your Favorites'},
   ];
   int _selectedPageIndex = 0;
 
@@ -28,9 +28,9 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
